637149d14b5e4a840d931364b7bf2b566bc9fb58
Vinícius Jardim <viniciuspjardim@gmail.com>
2018-07-08 01:51:31 -0300

    Criando mecanismo de busca e exibição dos arquivos

--- src/cache.ts
+++ src/cache.ts
@@ -1,12 +1,28 @@
+import { atualizarWebView } from "./extension";
+
 let fs = require('fs');
+let iconv = require('iconv-lite');
+let caminhoBase = "C:/folha/execucao/Emp_01_TOCANTINS";
 
 export class Contexto {
+
     public ambiente: string = "";
     public servidorCalculo: string = "";
     public tipoCalculo: string = "";
-    public mesAnoFol: number = 0;
+    public mesAnoFol: Date = new Date();
     public numFol: number = 0;
     public execucao: number = 0;
+
+    // func: 85409, vinc: 1, ano: 2016, mes: 6, seq_func: 2, seq_vinc: 2, rubrica: 1001, complemento: null, periodo: 2, tipo: "ini"
+    public numFunc: number = 0;
+    public numVinc: number = 0;
+    public mesAnoRub: Date = new Date();
+    public seqFunc: number = 0;
+    public seqVinc: number = 0;
+    public rubrica: number = 0;
+    public complemento: string = "";
+    public periodo: number = 0;
+    public tipo: string = "";
 }
 
 export class Cache {
@@ -41,14 +57,24 @@ export class ItemCache {
 
 export let rubricasCache: Cache = new Cache();
 
-export function parseArqRubricas() {
-    
-    let caminho: string = "C:/folha/execucao/Emp_01_TOCANTINS/Destino_delta_SRVFVJ/VJ/F_201604034_E00001/Debug/Folha12-NF001245988/SF02-NV03-SV01-Ano2016/teste.txt"; // SF02-NV03-SV01-Mes201604-SM01.dbg";
+export function parseArqRubricas(c: Contexto) {
 
-    fs.readFile(caminho, 'utf8', function (err: any, data: string) {
+    // C:/folha/execucao/Emp_01_TOCANTINS/Destino_delta        _SRVFVJ              /VJ              /F_2016                        04                               034                _E00001                /Debug/Folha12-NF001245988           /SF02                  -NV03                  -SV01                  -Ano2016                        /SF02                  -NV03                  -SV01                  -Mes2016                        04                               -SM01.dbg
+    let caminho: string = `${caminhoBase}/Destino_${c.ambiente}_${c.servidorCalculo}/${c.tipoCalculo}/F_${c.mesAnoFol.getFullYear()}${pad(c.mesAnoFol.getMonth(), 2)}${pad(c.numFol, 3)}_E${pad(c.execucao, 5)}/Debug/Folha12-NF${pad(c.numFunc, 9)}/SF${pad(c.seqFunc, 2)}-NV${pad(c.numVinc, 2)}-SV${pad(c.seqVinc, 2)}-Ano${c.mesAnoRub.getFullYear()}/SF${pad(c.seqFunc, 2)}-NV${pad(c.numVinc, 2)}-SV${pad(c.seqVinc, 2)}-Mes${c.mesAnoRub.getFullYear()}${pad(c.mesAnoRub.getMonth(), 2)}-SM01.dbg`;
+
+    console.log(caminho);
+
+    fs.readFile(caminho, 'binary', function (err: any, data: string) {
         if (err) {
+            atualizarWebView(true, "");
             return console.error(err);
         }
-        console.log("Asynchronous read: " + data.toString());
+        let conteudo: string = iconv.decode(data, '1252');
+        atualizarWebView(false, conteudo);
     });
+}
+
+export function pad(num: number, padlen: number): string {
+    let pad = new Array(1 + padlen).join('0');
+    return (pad + num).slice(-pad.length);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { parseArqRubricas } from './cache';
+import { parseArqRubricas, Contexto } from './cache';
+
+var panel: any;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,12 +20,98 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.carregarArquivos', () => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Carregando arquivos...');
-        parseArqRubricas();
+        let c: Contexto = new Contexto();
+
+        c.ambiente = "delta";
+        c.servidorCalculo = "SRVFVJ";
+        c.tipoCalculo = "VJ";
+        c.mesAnoFol = new Date(2016, 4, 1);
+        c.numFol = 34;
+        c.execucao = 1;
+
+        c.numFunc = 1245988;
+        c.numVinc = 3;
+        c.mesAnoRub = new Date(2016, 5, 1);
+        c.seqFunc = 2;
+        c.seqVinc = 1;
+        c.rubrica = 1001;
+        c.complemento = "";
+        c.periodo = 0;
+        c.tipo = "entra";
+        
+        parseArqRubricas(c);
+        
+        // Create and show a new webview
+        panel = vscode.window.createWebviewPanel(
+            'rubricasPeriodo', // Identifies the type of the webview. Used internally
+            "Rubricas Periodo", // Title of the panel displayed to the user
+            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+            { enableScripts: true } // Webview options. More on these later.
+        );
+
+        // And set its HTML content
+        panel.webview.html = criarWebView();
     });
 
     context.subscriptions.push(disposable);
+}
+
+export function atualizarWebView(erro: boolean, conteudo: string) {
+    if(erro) {
+        conteudo = "Falha ao carregar arquivo";
+    }
+    panel.webview.postMessage({ erro: erro, conteudo: conteudo });
+}
+
+export function criarWebView(): string {
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Rubricas Periodo</title>
+        <style>
+            body {
+                margin: 10px 5px 0px 0px;
+            }
+            .arquivo {
+                font-size: 16px;
+            }
+</style>
+    </head>
+    <body>
+        <form>
+            <div>
+                <samp>Ambiente<input id="ambiente"/></samp>
+                <samp>ServCalc<input id="servidorCalculo"/></samp>
+                <samp>TipoCalc<input id="tipoCalculo"/></samp>
+                <samp>MesAnoFol<input id="mesAnoFol"/></samp>
+                <samp>NumFol<input id="numFol"/></samp>
+                <samp>Exec<input id="execucao"/></samp>
+                <samp>Func<input id="numFunc"/></samp>
+                <samp>Vinc<input id="numVinc"/></samp>
+                <samp>MesAno<input id="mesAnoRub"/></samp>
+                <samp>SeqFunc<input id="seqFunc"/></samp>
+                <samp>SeqVinc<input id="seqVinc"/></samp>
+                <samp>Rub<input id="rubrica"/></samp>
+                <samp>Cpl<input id="complemento"/></samp>
+                <samp>Per<input id="periodo"/></samp>
+                <samp>Tipo<input id="tipo"/></samp>
+                <button type="button">Ok</button>
+            </div>
+        </form>
+        <div class="arquivo">
+            <pre id="conteudo">Nada para exibir</pre>
+        </div>
+        <script>
+            // Handle the message inside the webview
+            window.addEventListener('message', event => {
+                const message = event.data; // The JSON data our extension sent
+                document.getElementById("conteudo").innerHTML = message.conteudo; 
+            });
+        </script>
+    </body>
+    </html>`;
 }
 
 // this method is called when your extension is deactivated
